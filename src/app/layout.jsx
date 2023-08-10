f929df22c6be14bcaed1d79dc71d8e4039decad6
Fer Mérida <fermeridamagni@icloud.com>
2023-08-10 11:59:39 -0600

    Bootstrap and jQuery added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,8 @@
+import Script from 'next/script';
+
+import '@/resources/lib/bootstrap.min.css';
 import '@/resources/styles/globals.css';
+
 import { fontText } from '@/resources/fonts/fonts.config';
 
 export const metadata = {
@@ -11,7 +15,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={fontText.variable}>{children}</body>
+      <body className={fontText.variable}>
+        {children}
+
+        <Script src="/resources/lib/jQuery/jQuery.min.js"></Script>
+        <Script src="https://kit.fontawesome.com/3d7356b300.js"></Script>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" strategy="lazyOnload"></Script>
+      </body>
     </html>
   );
 };
