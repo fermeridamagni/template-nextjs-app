5c001e43f56402687be3acc0783251a8af41ffac
Fer Mérida <fermeridamagni@outlook.com>
2023-10-10 00:37:11 -0600

    commit 10/10/23

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,13 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
 
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-text)'],
-        sans: ['var(--font-title)']
+        text: ['var(--font-text)'],
+        title: ['var(--font-title)']
       },
     }
   },
@@ -20,4 +19,4 @@ module.exports = {
   plugins: [
     require('tailwindcss-animated')
   ],
-}
+};
